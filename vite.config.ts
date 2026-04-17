67082814a5b75b8df98102d07f2c5c6c8a2ed51c
ayus9818 <ayush.kc002@gmail.com>
2026-04-17 23:59:46 +0200

    Add Tailwind plugin for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import netlify from "@netlify/vite-plugin-tanstack-start"
 import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  base: "/",
   plugins: [
     tsconfigPaths(),
+    tailwindcss(),
     tanstackStart(),
     netlify(),
   ],
-  base: "/",
 })
